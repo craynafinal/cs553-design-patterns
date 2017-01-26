ce2e7abcca550e96862ae8fe1a9fd323d1b4a9c1
jsl <jsl@pdx.edu>
2017-01-25 21:49:03 -0800

    proxy updated

--- marker/part2/ComparableIF.java
+++ marker/part2/ComparableIF.java
@@ -4,12 +4,12 @@
  * <EM>java.lang.Comparable<EM>.
  */
 public interface ComparableIF {
-    /**
-     *  Compare two objects.
-     *  @param x The object to compare against this object.
-     *  @return The result of the comparison, which is either
-     *          true or false depending on both the objects <B>and</B>
-     *          the logic of the application.
-     */
-    boolean compare (ComparableIF x);
+	/**
+	 *  Compare two objects.
+	 *  @param x The object to compare against this object.
+	 *  @return The result of the comparison, which is either
+	 *          true or false depending on both the objects <B>and</B>
+	 *          the logic of the application.
+	 */
+	boolean compare (ComparableIF x);
 }
