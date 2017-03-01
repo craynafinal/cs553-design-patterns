dd039c38d90ca60105da0be0fc505b6a8ea33a38
jsl <jsl@pdx.edu>
2017-03-01 12:16:35 -0800

    fixing indentation for mediator

--- mediator/Main.java
+++ mediator/Main.java
@@ -7,19 +7,19 @@ import java.applet.*;
  *  Othello</A>.
  */
 public class Main extends Applet {
-    /**  Initialize the applet. */
-    public void init () { 
-	add (new Board ());
-	add (new Label ("Black moves first"));
-    }
+	/**  Initialize the applet. */
+	public void init () { 
+		add (new Board ());
+		add (new Label ("Black moves first"));
+	}
 
-    public static void main (String [] args) { 
-	Frame frame = new Frame ("Othello");
-	frame.add ("North", new Board ());
-	frame.add ("South", new Label ("Black moves first"));
-	frame.pack ();
-	frame.show ();
-	frame.setResizable (false);
-    }
+	public static void main (String [] args) { 
+		Frame frame = new Frame ("Othello");
+		frame.add ("North", new Board ());
+		frame.add ("South", new Label ("Black moves first"));
+		frame.pack ();
+		frame.show ();
+		frame.setResizable (false);
+	}
 }
 
